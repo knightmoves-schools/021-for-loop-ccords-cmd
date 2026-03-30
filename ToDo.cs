45aeb4c7cfa4a646ab26a1bdb69cff6071cbe844
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-30 14:18:22 -0500

    Fix MarkAsDone method return type to string array

--- ToDo.cs
+++ ToDo.cs
@@ -1,7 +1,7 @@
 ﻿namespace knightmoves;
 public class ToDo
 {
-public string MarkAsDone(string[] todos){
+public string[] MarkAsDone(string[] todos){
     string[] result = new string[todos.Length];
     for (int i = 0; i < todos.Length; i++){
         result[i] = "done - " + todos[i];
