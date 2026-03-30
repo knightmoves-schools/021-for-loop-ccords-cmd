c9c54841de0eabb7a8355419e61aa201b17918a5
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-30 14:12:34 -0500

    Fix formatting and syntax in ToDo class

--- ToDo.cs
+++ ToDo.cs
@@ -4,7 +4,7 @@ public class ToDo
 public string MarkAsDone(string[] todos){
     string[] result = new string[todos.length];
     for (int i = 0; i < todos.length; i++){
-        result[i] = "done -" + todos[i];
+        result[i] = "done - " + todos[i];
     }
     return result;
 }
